3031de9c2c33d9b27672635ad6d13431e8143a8c
arielrivera <rivera.ariel@gmail.com>
2024-08-25 08:17:31 -0500

    Update flarebrun.sh

--- flarebrun.sh
+++ flarebrun.sh
@@ -4,15 +4,10 @@ CERT_DIR="./certs"
 CERT_FILE="$CERT_DIR/localhost.crt"
 KEY_FILE="$CERT_DIR/localhost.key"
 
-
-# Stop and remove the existing containers
-docker stop cputrackerapp nginx4cputrackapp
-docker container rm cputrackerapp nginx4cputrackapp
-
 # Path to the database file on the host machine
 DATABASE_PATH=$(pwd)/cputracker.db
 
-# The folder that contains logs
+# The folder that contains your .7z logs
 TARGET_FOLDER="/Users/arivera/Downloads/processed_logs"
 
 # Target folder exists or not, prompt or use temp folder
@@ -42,21 +37,51 @@ if [ ! -d "$TARGET_FOLDER" ]; then
     fi
   fi
 fi
+# ----------------------------------------------------------
+#      Docker
+# ----------------------------------------------------------
+
+# Stop and remove the existing containers
+if docker ps -a | grep -q cputrackerapp; then
+  docker stop cputrackerapp
+  docker container rm cputrackerapp
+fi
+if docker ps -a | grep -q nginx4cputrackapp; then
+  docker stop nginx4cputrackapp
+  docker container rm nginx4cputrackapp
+fi
 
 # Remove all images
-docker rmi cputrackerapp_image nginx_image
+if docker images | grep -q cputrackerapp_image; then
+  docker rmi cputrackerapp_image
+fi
+if docker images | grep -q nginx_image; then
+  docker rmi nginx_image
+fi
 
-# Build the Docker images
+# Build the images
 docker build -t cputrackerapp_image -f Dockerfile.flask .
 docker build -t nginx_image -f Dockerfile.nginx .
 
-# Create the network if it doesn't exist
+# Create the container network if it doesn't exist
 if ! docker network ls | grep -q cputracker_network; then
   docker network create cputracker_network
 fi
 
 # Start the Flask container
-docker run -d --name cputrackerapp --network cputracker_network -v "$DATABASE_PATH":/app/cputracker.db -v "$TARGET_FOLDER":/app/logs_folder cputrackerapp_image
+docker run \
+-d --name cputrackerapp \
+--network cputracker_network \
+-v "$DATABASE_PATH":/app/cputracker.db \
+-v "$TARGET_FOLDER":/app/logs_folder \
+cputrackerapp_image
+
 
 # Start the Nginx container
-docker run -d --name nginx4cputrackapp --network cputracker_network -p 80:80 -p 443:443 -v "$CERT_FILE":/etc/nginx/certs/localhost.crt -v "$KEY_FILE":/etc/nginx/certs/localhost.key nginx_image
+docker run \
+-d --name nginx4cputrackapp \
+--network cputracker_network \
+-p 80:80 -p 443:443 \
+-v "$CERT_FILE":/etc/nginx/certs/localhost.crt \
+-v "$KEY_FILE":/etc/nginx/certs/localhost.key \
+nginx_image
